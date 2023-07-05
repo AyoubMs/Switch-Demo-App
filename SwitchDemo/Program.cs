66d05a67e8b309fc95c48be2bca0597130f6db8e
AyoubMS <chahayb563@gmail.com>
2023-07-05 15:33:42 +0200

    add app

---
+++ SwitchDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+string firstName = "Tim";
+int age = 13;
+
+switch (age)
+{
+    case >= 0 and < 18:
+        Console.WriteLine("You are a child");
+        break;
+    default:
+        Console.WriteLine("Age was not in an expected range.");
+        break;
+}
